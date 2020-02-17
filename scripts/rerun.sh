404b08a3fc9ab8cce28faf42b457a1a98f72179d
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-17 12:20:21 +0100

    Increase time on Peregrine [skip ci]

--- scripts/rerun.sh
+++ scripts/rerun.sh
@@ -7,6 +7,15 @@
 #   ./scripts/rerun.sh
 #
 #
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
+#SBATCH --mem=10G
+#SBATCH --job-name=pirouette_example_29
+#SBATCH --output=pirouette_example_29.log
+module load R
+
 rm -rf example_29_314
 Rscript example_29.R
 
