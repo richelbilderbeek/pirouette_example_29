28d7d94f0d63c6dfaa56acaee926c28d948c189f
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 20:38:55 +0100

    Use of 'pir_save'

--- example_29.R
+++ example_29.R
@@ -7,7 +7,6 @@
 # reasonably small trees (say in the magnitudes of hundreds of them)
 # with 10:40 taxa
 suppressMessages(library(pirouette))
-suppressMessages(library(ggplot2))
 library(testthat)
 
 ################################################################################
@@ -113,6 +112,7 @@ pir_outs <- pir_runs(
 for (i in seq_along(pir_outs)) {
   pir_save(
     phylogeny = phylogenies[[i]],
+    pir_params = pir_paramses[[i]],
     pir_out = pir_outs[[i]],
     folder_name = folder_names[[i]]
   )
