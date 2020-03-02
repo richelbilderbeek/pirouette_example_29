479485727f5cd3dcc0350e9f249371ab3d93cfb5
richelbilderbeek <richel@richelbilderbeek.nl>
2020-03-02 14:35:40 +0100

    Code cleaner

--- example_29.R
+++ example_29.R
@@ -6,76 +6,49 @@
 # Write script that shows the true and twin error for a fixed tree prior and
 # reasonably small trees (say in the magnitudes of hundreds of them)
 # with 10:40 taxa
-suppressMessages(library(pirouette))
-library(testthat)
 
 # Constants
-is_testing <- is_on_travis()
 example_no <- 29
-n_replicates <- 5
-unique_mcmc_chain_lengths <- c(1e6, 1e7, 1e8)
+rng_seed <- 314
 crown_age <- 10
-n_taxa <- 6
+mcmc_chain_lengths <- c(1e5, 1e6, 1e7, 1e8)
+n_phylogenies_per_mcmc_chain_length <- 5
 is_testing <- is_on_travis()
-# Number of replicates per number of taxa
 if (is_testing) {
-  n_replicates <- 2
-  root_folder <- tempdir()
-  unique_mcmc_chain_lengths <- c(3000, 4000)
+  mcmc_chain_lengths <- c(3000, 4000)
+  n_phylogenies_per_mcmc_chain_length <- 2
 }
-rng_seeds <- seq(314, 314 - 1 + length(unique_mcmc_chain_lengths) * n_replicates)
-mcmc_chain_lengths <- rep(unique_mcmc_chain_lengths, each = n_replicates)
-expect_equal(length(rng_seeds), length(mcmc_chain_lengths))
+n_mcmc_chain_lengths <- length(mcmc_chain_lengths)
+n_pir_params <- n_mcmc_chain_lengths * n_phylogenies_per_mcmc_chain_length
+
 # Create phylogenies
 phylogenies <- list()
-for (i in seq_along(rng_seeds)) {
-  rng_seed <- rng_seeds[i]
+for (i in seq_len(n_phylogenies_per_mcmc_chain_length)) {
   set.seed(rng_seed)
-  phylogenies[[i]] <- create_yule_tree(
-    n_taxa = n_taxa,
-    crown_age = crown_age
-  )
+  phylogeny <- create_yule_tree(n_taxa = 6, crown_age = 10)
+  phylogenies[[i]] <- phylogeny
 }
-expect_equal(length(phylogenies), length(rng_seeds))
+# 1 2 3 1 2 3
+phylogenies <- rep(phylogenies, n_phylogenies_per_mcmc_chain_length)
+
 # Create pirouette parameter sets
-pir_paramseses <- create_std_pir_params(n = length(phylogenies))
+pir_paramses <- create_std_pir_paramses(n = n_pir_params)
 expect_equal(length(pir_paramses), length(phylogenies))
-# Shorter run on Travis
 if (is_testing) {
-  pir_paramses <- shorten_pir_paramses(pir_paramses)
+  pir_paramses <- shorten_pir_params(pir_paramses)
 }
-# Set the RNG seeds
-pir_paramses <- renum_rng_seeds(
-  pir_paramses = pir_paramses,
-  rng_seeds = seq(314, 314 - 1 + length(pir_paramses))
+
+# Set the alignment lengths
+# 1 1 1 2 2 2
+mcmc_chain_lengthses <- rep(
+  mcmc_chain_lengths, each = n_phylogenies_per_mcmc_chain_length
 )
-
-# Rename filenames
-for (i in seq_along(pir_paramses)) {
-  rng_seed <- pir_paramses[[i]]$alignment_params$rng_seed
-  pir_paramses[[i]] <- pir_rename_to_std(
-    pir_params = pir_paramses[[i]],
-    folder_name = file.path(root_folder, paste0("example_", example_no, "_", rng_seed))
-  )
-}
-
-# Save tree to files
-for (i in seq_along(pir_paramses)) {
-  expect_equal(length(pir_paramses), length(phylogenies))
-  rng_seed <- pir_paramses[[i]]$alignment_params$rng_seed
-  folder_name <- file.path(root_folder, paste0("example_", example_no, "_", rng_seed))
-  # Create folder, do not warn if it already exists
-  dir.create(folder_name, showWarnings = FALSE, recursive = TRUE)
-  ape::write.tree(
-    phylogenies[[i]],
-    file = file.path(folder_name, "true_tree.newick")
-  )
-}
-
-# Delete previous files
-for (pir_params in pir_paramses) {
-  check_pir_params(pir_params)
-  rm_pir_param_files(pir_params)
+expect_equal(length(mcmc_chain_lengthses), length(pir_paramses))
+for (i in seq_along(mcmc_chain_lengthses)) {
+  for (j in seq_along(pir_paramses[[i]]$experiments)) {
+    pir_paramses[[i]]$experiments[[j]]$inference_model$mcmc$chain_length <-
+      mcmc_chain_lengthses[[i]]
+  }
 }
 
 # Do the runs
@@ -85,12 +58,13 @@ pir_outs <- pir_runs(
 )
 
 # Save
+expect_equal(length(pir_paramses), length(pir_outs))
+expect_equal(length(pir_paramses), length(phylogenies))
 for (i in seq_along(pir_outs)) {
   pir_save(
     phylogeny = phylogenies[[i]],
     pir_params = pir_paramses[[i]],
     pir_out = pir_outs[[i]],
-    folder_name = folder_names[[i]]
+    folder_name = dirname(pir_paramses[[i]]$alignment_params$fasta_filename)
   )
 }
-
